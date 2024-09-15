07ee0d879ca3351e6d8ea2141729e69b1d1caea9
rpc1418 <rudrakshchourey@outlook.com>
2024-09-15 12:40:21 +0530

    Add Flask setup

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 print("jai shree ganesh")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return {'status': 'API is active'}, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
